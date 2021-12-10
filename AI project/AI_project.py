1deeb4f4d9ab5ba15bb3ff7622022ab21dfcf8ce
mina x <mina20191702074@cis.asu.edu.eg>
2021-12-10 13:09:17 +0200

    last

--- AI project/AI_project.py
+++ AI project/AI_project.py
@@ -19,20 +19,7 @@ def Feature_Encoder(X, cols):
     return X
 
 
-def removeUnnecessaryColumns():
-    global data
-    data = data.drop(['id'], axis=1)
-    data = data.drop(['imdb_id'], axis=1)
-    data = data.drop(['director'], axis=1)
-    data = data.drop(['original_title'], axis=1)
-    data = data.drop(['cast'], axis=1)
-    data = data.drop(['homepage'], axis=1)
-    data = data.drop(['tagline'], axis=1)
-    data = data.drop(['keywords'], axis=1)
-    data = data.drop(['overview'], axis=1)
-    data = data.drop(['production_companies'], axis=1)
-    data = data.drop(['release_date'], axis=1)
-    data = data.drop(['release_year'], axis=1)
+data.drop(['id','imdb_id','original_title','homepage','tagline','director','overview','cast','production_companies','release_year','keywords'],axis=1,inplace=True)
 
 
 def cleanData():
@@ -63,56 +50,12 @@ def normalizeData():
 # convert categorical columns into numerical using Feature_Encoder
 data = Feature_Encoder(data, ['genres'])
 
-removeUnnecessaryColumns()
 cleanData()
 normalizeData()
-
-
-
-
-
 print(data.isnull().sum())
 print(data.columns)
-
 #print first 5 rows
 print(data.head())
 
 
-X = data['revenue_adj']
-Y = data['budget_adj']
-print(X.shape)
 
-# plt.scatter(X, Y)
-# plt.xlabel('revenue', fontsize = 20)
-# plt.ylabel('budget', fontsize = 20)
-# plt.show()
-
-# def Predictive_Line(X, Theta):
-
-#    Predictions = None
-#    X = np.append(np.ones((1,X.shape[0])), X.reshape((1,X.shape[0])), axis = 0)
-
-
-#    Predictions = np.dot(Theta.T, X)
-
-
-#    Predictions = Predictions.T
-#    return Predictions
-
-# def Calculate_Cost(X, Theta, Y):
-#    m = Y.shape[0]
-#    J = 0
-
-#    h = Predictive_Line(X, Theta)
-#    J = ((1/(2 * m)) * np.sum(np.square(h - Y), axis = 0))
-
-#    return J
-
-# def Gradient_Descent(X, Y, Theta, alpha, num_iters):
-
-#    m = Y.shape[0]
-#    X1 = np.append(np.ones(X.shape) , X, axis = 1)
-#    for i in range(num_iters):
-#        h = Predictive_Line(X, Theta.reshape((2,1)))
-#        Theta = Theta - (alpha / m) * ((h - Y).T).dot(X1)
-#    return Theta
