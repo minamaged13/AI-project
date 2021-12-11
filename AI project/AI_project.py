d51883fe221b0f1cb28bba13f5b5b989669d78bd
Marwan-Ezzat-Mohamed <marwan.ezzat.badawi@gmail.com>
2021-12-11 20:26:13 +0200

    project 48al

--- AI project/AI_project.py
+++ AI project/AI_project.py
@@ -3,18 +3,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv('tmdb_movies_data.csv')
 df.head()
 df.info()
 df['director'].value_counts()
 df.drop(['id', 'imdb_id', 'original_title', 'homepage', 'tagline', 'director', 'overview',
-        'cast', 'production_companies', 'release_year','release_date', 'keywords','budget','revenue'], axis=1, inplace=True)
+        'cast', 'production_companies', 'release_year', 'release_date', 'keywords', 'budget', 'revenue'], axis=1, inplace=True)
 
 df['net_profit'] = df['revenue_adj'] - df['budget_adj']
 
 
-x = df.drop(['net_profit'], axis=1)
+def Feature_Encoder(X, cols):
+    for c in cols:
+        lbl = LabelEncoder()
+        # nationality, club, position (convert them to numbers 0- nclass-1)
+        lbl.fit(list(X[c].values))
+        X[c] = lbl.transform(list(X[c].values))
+    return X
+
+df = Feature_Encoder(df, ['genres'])
+
+
+x = df.drop(['net_profit', 'revenue_adj'], axis=1)
 y = df['net_profit']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
