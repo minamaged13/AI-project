d40256c258fd98c76f91fbe4bc8d14c7d71e6e2a
Marwan-Ezzat-Mohamed <marwan.ezzat.badawi@gmail.com>
2021-12-11 19:25:43 +0200

    code gded

--- AI project/AI_project.py
+++ AI project/AI_project.py
@@ -1,61 +1,91 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
-iterations = 100
-data = pd.read_csv('tmdb_movies_data.csv')
-print(data.columns)
-
-# add new column for net_profit
-data['net_profit'] = data['revenue_adj'] - data['budget_adj']
-
-def Feature_Encoder(X, cols):
-    for c in cols:
-        lbl = LabelEncoder()
-        # nationality, club, position (convert them to numbers 0- nclass-1)
-        lbl.fit(list(X[c].values))
-        X[c] = lbl.transform(list(X[c].values))
-    return X
+df = pd.read_csv('tmdb_movies_data.csv')
+df.head()
+df.info()
+df['director'].value_counts()
+df.drop(['id', 'imdb_id', 'original_title', 'homepage', 'tagline', 'director', 'overview',
+        'cast', 'genres', 'production_companies', 'release_year', 'keywords'], axis=1, inplace=True)
+df['release_date'].value_counts()
 
 
-data.drop(['id','imdb_id','original_title','homepage','tagline','director','overview','cast','production_companies','release_year','keywords'],axis=1,inplace=True)
+def extract_day(day):
+    return day.split('/')[0]
 
 
-def cleanData():
-    global data
-    # remove null values
-    data = data.dropna()
-    # remove duplicates
-    data = data.drop_duplicates()
-
-    # Filter and clean the columns and rows
-    data = data[data['vote_average'] > 0]
-    data = data[data['vote_count'] > 0]
-    data = data[data['popularity'] > 0]
-    data = data[data['revenue'] > 0]
-    data = data[data['runtime'] > 0]
-    data = data[data['budget'] > 0]
-    data = data[data['genres'] != '[]']
+def extract_month(month):
+    return month.split('/')[1]
 
 
-def normalizeData():
-    global data
-    # normalize the data
-    data['revenue'] = data['revenue'] / 1000000000
-    data['budget'] = data['budget'] / 1000000000
-    data['popularity'] = data['popularity'] / 100
-    data['runtime'] = data['runtime'] / 60
+df['Day'] = df['release_date'].apply(extract_day)
+df['Month'] = df['release_date'].apply(extract_month)
 
-# convert categorical columns into numerical using Feature_Encoder
-data = Feature_Encoder(data, ['genres'])
+df.drop('release_date', axis=1, inplace=True)
 
-cleanData()
-normalizeData()
-print(data.isnull().sum())
-print(data.columns)
-#print first 5 rows
-print(data.head())
+x = df.drop(['net_profit', 'revenue_adj', 'budget_adj'], axis=1)
+y = df['net_profit']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+scaler = StandardScaler()
+
+scaler.fit(x_train)
+
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
 
 
+y_train = np.array(y_train)
+y_train = y_train.reshape(y_train.shape[0], 1)
 
+y_test = np.array(y_test)
+y_test = y_test.reshape(y_test.shape[0], 1)
+
+x_train = np.vstack((np.ones((x_train.shape[0], )), x_train.T)).T
+x_test = np.vstack((np.ones((x_test.shape[0], )), x_test.T)).T
+
+print("X_train :", x_train.shape)
+print("X_test :", x_test.shape)
+print("y_train :", y_train.shape)
+print("y_test :", y_test.shape)
+
+
+def model(x, y, learning_rate, iteration):
+    m = y.size
+    theta = np.zeros((x.shape[1], 1))
+    cost_ = []
+
+    for i in range(iteration):
+        y_pred = np.dot(x, theta)
+        cost = (1/(2*m))*np.sum(np.square(y_pred-y))
+
+        d_theta = (1/m)*np.dot(x.T, y_pred-y)
+        theta = theta-learning_rate*d_theta
+
+        cost_.append(cost)
+
+        if((i % iteration/10) == 0):
+            print("Cost :", cost)
+
+    return theta, cost_
+
+
+def Gradient_Descent(X, Y, Theta, alpha, num_iters):
+
+    m = Y.shape[0]
+
+    X1 = np.append(np.ones(X.shape), X, axis=1)
+    for i in range(num_iters):
+        h = Predictive_Line(X, Theta.reshape((2, 1)))
+        Theta = Theta - (alpha / m) * ((h - Y).T).dot(X1)
+
+    return Theta
+
+
+iteration = 1000
+learning_rate = 0.0000005
+theta, cost_ = model(
+    x_train, y_train, learning_rate=learning_rate, iteration=iteration)
