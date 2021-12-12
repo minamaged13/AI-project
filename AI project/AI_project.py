e82c3c0798373ca90d79ab14498d7395e17a6de1
mina x <mina20191702074@cis.asu.edu.eg>
2021-12-12 16:42:25 +0200

    cost preblem

--- AI project/AI_project.py
+++ AI project/AI_project.py
@@ -7,12 +7,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv('tmdb_movies_data.csv')
-df.head()
-df.info()
-df['director'].value_counts()
 df.drop(['id', 'imdb_id', 'original_title', 'homepage', 'tagline', 'director', 'overview',
         'cast', 'production_companies', 'release_year', 'release_date', 'keywords', 'budget', 'revenue'], axis=1, inplace=True)
-
 df['net_profit'] = df['revenue_adj'] - df['budget_adj']
 
 
@@ -27,7 +23,8 @@ def Feature_Encoder(X, cols):
 df = Feature_Encoder(df, ['genres'])
 
 
-x = df.drop(['net_profit', 'revenue_adj'], axis=1)
+
+x = df.drop(['net_profit'], axis=1)
 y = df['net_profit']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
@@ -48,44 +45,88 @@ y_test = y_test.reshape(y_test.shape[0], 1)
 x_train = np.vstack((np.ones((x_train.shape[0], )), x_train.T)).T
 x_test = np.vstack((np.ones((x_test.shape[0], )), x_test.T)).T
 
-print("X_train :", x_train.shape)
-print("X_test :", x_test.shape)
-print("y_train :", y_train.shape)
-print("y_test :", y_test.shape)
 
+print("Shape of X_train :", x_train.shape)
+print("Shape of Y_train :", y_train.shape)
+print("Shape of X_test :", x_test.shape)
+print("Shape of Y_test :", y_test.shape)
 
-def cost(x,y,theta):
-    m=x.shape[0]
-    prediction=x.dot(theta)
-    error=np.square(np.subtract(prediction,y))
-    
-    j=1/(2*m)* np.sum(error)
-    return j
+def model(X, Y, learning_rate, iteration):
 
-def gradient_descent(x,y,theta,alpha,iteration):
-    m=x.shape[0]
-    cost_=np.zeros(iteration)
-    
+    m = Y.size
+    theta = np.zeros((X.shape[1], 1))
+    cost_list = []
     for i in range(iteration):
-        prediction=np.dot(x,theta)
-        errors=np.subtract(prediction,y)
-        delta=(alpha/m)*x.T.dot(errors)
-        theta=theta-delta
+
+        y_pred = np.dot(X, theta)
+        cost = (1/(2*m))*np.sum(np.square(y_pred - Y))
+        d_theta = (1/(m))*np.dot(X.T, y_pred - Y)
+        theta = theta - learning_rate*d_theta
+        cost_list.append(cost)
+        # to print the cost for 10 times
+       # if(i%(iteration/1000) == 0):
+            #print("Cost is :", cost)
+    return theta, cost_list
+
+iteration = 1000
+learning_rate = 0.000005
+#theta, cost_list = model(x_train, y_train, learning_rate = learning_rate, iteration =iteration)
+
+#rng = np.arange(0, iteration)
+#plt.plot(rng, cost_list)
+#plt.show()
+#y_pred = np.dot(x_test, theta)
+#error = (1/x_test.shape[0])*np.sum(np.abs(y_pred - y_test))
+#print("Test error is :", error*100, "%")
+#print("Test Accuracy is :", (1- error)*100, "%")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def cost(x,y,theta):
+#    m=x.shape[0]
+#    prediction=x.dot(theta)
+#    error=np.square(np.subtract(prediction,y))
+    
+#    j=1/(2*m)* np.sum(error)
+#    return j
+
+#def gradient_descent(x,y,theta,alpha,iteration):
+#    m=x.shape[0]
+#    cost_=np.zeros(iteration)
+    
+#    for i in range(iteration):
+#        prediction=np.dot(x,theta)
+#        errors=np.subtract(prediction,y)
+#        delta=(alpha/m)*x.T.dot(errors)
+#        theta=theta-delta
         
-        cost_[i]=cost(x,y,theta)
-        if((i%iteration/10)==0):
-            print("Cost :",cost)
-    return theta,cost_
+#        cost_[i]=cost(x,y,theta)
+#        if((i%iteration/10)==0):
+#            print("Cost :",cost)
+#    return theta,cost_
 
-theta= np.array(np.zeros(7)).reshape((1,7)).T
-iteration=400
-alpha=0.15
+#theta= np.array(np.zeros(7)).reshape((1,7)).T
+#iteration=400
+#alpha=0.15
 
-theta.shape
+#theta.shape
 
-theta,cost=gradient_descent(x_train,y_train,theta,alpha,iteration)
-print("cost",cost[:40])
-
-
-
-
+#theta,cost=gradient_descent(x_train,y_train,theta,alpha,iteration)
+#print("cost",cost[:40])
